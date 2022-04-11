0007b9ae7eef49e1552c77b01d54de8a3f97b3c2
Adrestia <56211146+Adrestia10@users.noreply.github.com>
2022-04-11 00:13:59 -0600

    here is the stuff

--- sagira/cogs/testcog.py
+++ sagira/cogs/testcog.py
@@ -1,5 +1,6 @@
+from cgitb import text
 import discord
-from discord import app_commands
+from discord import SelectOption, app_commands
 from discord.ext import commands
 from discord import ui
 
@@ -19,9 +20,9 @@ class Questionnaire(ui.Modal, title='Questionnaire Response'):
 
     async def on_submit(self, interaction: discord.Interaction):
         embed = discord.Embed(
-            title="Responses",
+            title="",
             description="",
-            colour=discord.Colour.green()
+            colour=discord.Colour.blue()
         )
         embed.add_field(
             name="Name",
@@ -33,7 +34,37 @@ class Questionnaire(ui.Modal, title='Questionnaire Response'):
             value=self.answer,
             inline=True
         )
-        await interaction.response.send_message(f'name={self.name}, answer={self.answer}', embed=embed)
+        view = ui.View()
+        view.add_item(JoinButton(
+            style=discord.ButtonStyle.primary,
+            label="Join",
+        ))
+        view.add_item(ui.Button(
+            style=discord.ButtonStyle.danger,
+            label="Leave",
+        ))
+        view.add_item(ui.Button(
+            style=discord.ButtonStyle.primary,  # TODO: can this take a URL?
+            label="Alternate",
+        ))
+        view.add_item(EditButton(
+            style=discord.ButtonStyle.danger,
+            label="Edit"
+        ))
+        view.add_item(ui.Select(
+            custom_id="select-menu",
+            placeholder='stuf',
+            min_values=1,
+            max_values=2,
+            options=[
+                SelectOption(label="option 1", value="option 1"),
+                SelectOption(label="option 2", value="option 2")
+            ]
+
+        ))
+        embed.set_footer(text= f"Author: {interaction.user.name}")
+        embed.set_thumbnail(url="https://www.destinypedia.com/images/thumb/8/89/Raid.jpg/250px-Raid.jpg",)
+        await interaction.response.send_message(embed=embed, view=view,)
 
 
 class EditModal(ui.Modal, title='Edit raid'):
@@ -44,7 +75,7 @@ class EditModal(ui.Modal, title='Edit raid'):
         embed = discord.Embed(
             title="Responses",
             description="",
-            colour=discord.Colour.green()
+            colour=discord.Colour.blue()
         )
         embed.add_field(
             name="Name",
@@ -56,45 +87,68 @@ class EditModal(ui.Modal, title='Edit raid'):
             value=self.answer,
             inline=True
         )
-        await interaction.response.edit_message(content=f'new_name={self.name}, new_answer={self.answer}', embed=embed)
-
+        embed.set_footer(text= f"Author: {interaction.user.name}") #todo edited by instead of author while also preserving original author
+        embed.set_thumbnail(url="https://www.destinypedia.com/images/thumb/8/89/Raid.jpg/250px-Raid.jpg",)
+        await interaction.response.edit_message(embed=embed)
 
 class EditButton(ui.Button):
     async def callback(self, interaction: discord.Interaction):
         await interaction.response.send_modal(EditModal())
+
+class JoinModal(ui.Modal,title='g'):
+     name = ui.TextInput(label='NewName') # do i need this
+     
+     async def on_submit(self, interaction: discord.Interaction):
+        embed = discord.Embed()
+        embed.add_field(
+            name="Name",
+            value=self.name,
+            inline=True
+        )
+        await interaction.response.edit_message(embed=embed)
+
+
+
+
+class JoinButton(ui.Button):
+    async def callback(self, interaction: discord.Interaction):
+        await interaction.response.send_modal(JoinModal())
+
 
 
 class TestCog(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    @app_commands.command(name="testcommand")
-    @app_commands.guilds(discord.Object(id=Config.discord_guild_id))
-    async def testcommand(self, interaction: discord.Interaction) -> None:
-        """some description"""
-        embed = discord.Embed(
-            title="some title",
-            description="",
-            colour=discord.Colour.green()
-        )
-        view = ui.View()
-        view.add_item(ui.Button(
-            style=discord.ButtonStyle.primary,
-            label="Join",
-        ))
-        view.add_item(ui.Button(
-            style=discord.ButtonStyle.secondary,
-            label="Leave",
-        ))
-        # view.add_item(ui.Button(
-        #     style=discord.ButtonStyle.link,  # TODO: can this take a URL?
-        #     label="Alternate",
-        # ))
-        view.add_item(EditButton(
-            style=discord.ButtonStyle.danger,
-            label="Edit"
-        ))
-        await interaction.response.send_message("here's some test output", embed=embed, view=view)
+    # @app_commands.command(name="make_event")
+    # @app_commands.guilds(discord.Object(id=Config.discord_guild_id))
+    # async def testcommand(self, interaction: discord.Interaction) -> None:
+    #     """some description"""
+        
+    #     embed = discord.Embed(
+    #         title="some title",
+    #         description="",
+    #         colour=discord.Colour.blue()
+    #     )
+    #     view = ui.View()
+    #     view.add_item(ui.Button(
+    #         style=discord.ButtonStyle.primary,
+    #         label="Join",
+    #     ))
+    #     view.add_item(ui.Button(
+    #         style=discord.ButtonStyle.danger,
+    #         label="Leave",
+    #     ))
+    #     view.add_item(ui.Button(
+    #         style=discord.ButtonStyle.primary,  # TODO: can this take a URL?
+    #         label="Alternate",
+    #     ))
+    #     view.add_item(EditButton(
+    #         style=discord.ButtonStyle.danger,
+    #         label="Edit"
+    #     ))
+    #     embed.set_thumbnail(url="https://www.destinypedia.com/images/thumb/8/89/Raid.jpg/250px-Raid.jpg",)
+    #     await interaction.response.send_message(embed=embed, view=view)
 
     @app_commands.command(name="modaltest")
     @app_commands.guilds(discord.Object(id=Config.discord_guild_id))
@@ -102,19 +156,19 @@ class TestCog(commands.Cog):
         """some modal test lol"""
         await interaction.response.send_modal(Questionnaire())
 
-    @app_commands.command(name="selecttest")
-    @app_commands.guilds(discord.Object(id=Config.discord_guild_id))
-    async def selecttest(self, interaction: discord.Interaction) -> None:
-        """some select test lol"""
-        modal = Questionnaire()
-        # select = ui.Select()
-        # select.add_option(
-        #     label="opt1",
-        #     value="val1",
-        #     description="description1"
-        # )
-        # modal.add_item(select)
-        await interaction.response.send_modal(modal)
+    # @app_commands.command(name="selecttest")
+    # @app_commands.guilds(discord.Object(id=Config.discord_guild_id))
+    # async def selecttest(self, interaction: discord.Interaction) -> None:
+    #     """some select test lol"""
+    #     modal = Questionnaire()
+    #     # select = ui.Select()
+    #     # select.add_option(
+    #     #     label="opt1",
+    #     #     value="val1",
+    #     #     description="description1"
+    #     # )
+    #     # modal.add_item(select)
+    #     #await interaction.response.send_modal(modal)
 
 
 async def setup(bot) -> None:
